306a02589217f2f6d6d3b7061df73f7099641943
Cuong-Dev-2004 <hoanghuuhanh321@gmail.com>
2025-11-25 18:47:34 +0700

    fix

--- src/Pages/Pages.jsx
+++
@@ -1,6 +0,0 @@
-import Booking from "./Booking/Booking.jsx";
-import HomePage from "./HomePage/HomePage.jsx";
-import OtherAi from "./OtherAi/OtherAi.jsx";
-import TourBookingFlow from "./TourBookingFlow/TourBookingFlow.jsx";
-
-export { HomePage, OtherAi, Booking, TourBookingFlow };
--- src/assets/Icon/Icon.jsx
+++
@@ -1,4 +0,0 @@
-export { AiOutlineRight } from "react-icons/ai";
-export { BsFillArrowRightCircleFill, BsFillArrowLeftCircleFill } from "react-icons/bs";
-export { FaFacebookMessenger } from "react-icons/fa";
-export { MdTour } from "react-icons/md";
